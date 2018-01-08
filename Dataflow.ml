7d12e1fb06f87ea02bc24d5ef8017d76eaf2eabe
Marius Bachibouzouk <marius.hinge@gmail.com>
2018-01-08 18:49:21 +0100

    Travail sur le Dataflow custom

--- Dataflow.ml
+++ Dataflow.ml
@@ -1,3 +1,5 @@
+open Cil_types;;
+
 let help_msg = "This is the Invariant Tab module"
 
 module Self = Plugin.Register
@@ -6,6 +8,55 @@ module Self = Plugin.Register
     let shortname = "IT"
     let help = help_msg
   end)
+
+(* Treillis *)
+(* TODO - Type & Union & Equal & Diff*)
+type Prop = 
+  | Top
+  | Bottom
+;;
+
+let union a b = 
+  match (a;b) with
+  | (Top,_) -> Top
+  | (_, Top) -> Top
+  | (Bottom, _) -> Bottom
+  | (_, Bottom) -> Bottom
+;;
+
+let diff a b = 
+  match (a;b) with
+  | (Top,Top) -> true
+  | (Top,_) -> false
+  | (Bottom, Bottom) -> true
+  | (Bottom, _) -> false
+;;
+
+(* Sémantique *)
+(*TODO - Type block (liste d'instruction de la boucle)*)
+let analyse_boucle block phi = 
+  Printer.pp_instr out block
+;;
+
+
+let rec looper phi_1 phi_2 block =  
+  if (diff phi_1 phi_2) then let phi = (union phi_1 phi_2) in looper (analyse_boucle block phi) phi
+  else phi_1
+;;
+
+let print_stmt out = function
+  | Instr i -> Printer.pp_instr out i
+  | Return _ -> Format.pp_print_string out "<return>"
+  | Goto _ -> Format.pp_print_string out "<goto>"
+  | Break _-> Format.pp_print_string out "<break>"
+  | Continue _-> Format.pp_print_string out "<continue>"
+  | If(e,_,_,_)-> Format.fprintf out "if %a" Printer.pp_exp e
+  | Switch(e,_,_,_)-> Format.fprintf out "switch %a" Printer.pp_exp e
+  | Loop _-> Format.fprintf out "<loop>"
+  | Block _-> Format.fprintf out "<block>"
+  | UnspecifiedSequence _-> Format.fprintf out "<unspecified sequence>"
+  | TryFinally _ | TryExcept _ | TryCatch _-> Format.fprintf out "<try>"
+  | Throw _-> Format.fprintf out"<throw>"
 
 let run () = 
   Self.result "Hello world!";
--- file.c
+++ file.c
@@ -4,6 +4,12 @@ void f (int g)
   g--;
 }
 
+void copy (int a[], int b[], int n){
+  for (int i=0; i<n; ++i){
+    a[i] = b[i];
+  }
+}
+
 int main(int argc, char** argv)
 {
   int i=3;
